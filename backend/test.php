ca90276604e7dcde9a8b0997f0517047049f8afd
joaoribeiro <joao.ribeiro@tray.net.br>
2022-09-02 21:34:03 -0300

    docker implementado db

--- backend/test.php
+++ backend/test.php
@@ -1,5 +1,16 @@
 <?php
-$conn = mysqli_connect("db","root", "root",'template_training') or die(mysqli_error());
+//$conn = mysqli_connect("db","root", "root",'template_training') or die(mysqli_error());
+
+$user = "root";
+$pass = "root";
+$host = "mysql-container";
+$dbdb = "template_training";
+
+$conn = new mysqli($host, $user, $pass, $dbdb);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
 echo '<h2> CONEXÃO REALIZADA COM SUCESSO </h2>';
-$conn->close();
+  $conn->close();
 
